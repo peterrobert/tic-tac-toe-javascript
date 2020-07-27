4281c1e02d5f8c345b31c44865fa719c903789ac
peterrobert <pwambui93@gmail.com>
2020-07-27 22:39:32 +0300

    created the game objects

---
+++ main.js
@@ -0,0 +1,29 @@
+function GameBoard() {
+    let gameboard  = ['','','','','','','','',''];
+
+
+    return {gameboard};
+}
+
+
+function Players(player_symbol) {
+
+   let pSymbol = player_symbol;
+
+   return{pSymbol}
+      
+}
+
+
+function GameLogic(playerOne, playerTwo, board) {
+    
+   function play() {
+       
+   }
+
+   function gameFinish() {
+       
+   }
+
+   return{play , gameFinish}
+}
