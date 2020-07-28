0e66fa5949253ee859bd6734a354dd4593ffccd4
peterrobert <pwambui93@gmail.com>
2020-07-28 19:36:56 +0300

    creating the game finish logic

--- main.js
+++ main.js
@@ -1,27 +1,37 @@
 let current_player;
 
 function GameBoard() {
-    let gameboard = ['', '', '', '', '', '', '', '', ''];
+    let gameboard = [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+    ];
 
     let render = () => {
         let container = document.querySelector('.container');
         let display = '';
         gameboard.forEach((el, i) => {
-            display += `
-         <div class="gameboard" data-index='${i}'>${el}</div>
+
+            el.forEach((el2, j) => {
+                display += `
+         <div class="gameboard" data-primary-index = '${i}' data-index='${j}'>${el2}</div>
          `;
+            });
+
         });
+
         container.innerHTML = display;
         document.querySelectorAll('.gameboard').forEach((el) => {
+            
             el.addEventListener('click', () => {
-                spot(el.getAttribute('data-index'));
+                spot(parseInt(el.getAttribute('data-index')), parseInt(el.getAttribute('data-primary-index')));
             });
         });
     }
 
-    let spot = (index) => {
-        if (gameboard[parseInt(index)] == '') {
-            gameboard[parseInt(index)] = current_player.pSymbol;
+    let spot = (index, primaryIndex) => {
+        if (gameboard[primaryIndex][index] == '') {
+            gameboard[primaryIndex][index] = current_player.pSymbol;
             game.alterPlayer(current_player);
             render();
         }
@@ -61,58 +71,11 @@ function GameLogic(playerOne, playerTwo, board) {
     }
 
     function gameFinish() {
-
-        let win = [
-            [0, 1, 2],
-            [3, 4, 5],
-            [6, 7, 8],
-            [0, 4, 8],
-            [2, 4, 6],
-            [0, 3, 6],
-            [1, 4, 7],
-            [2, 5, 8]
-        ];
-
-        let playerOneValues = [];
-        let playerTwoValues = [];
-        let winner = ''
-
-        if (!board.gameboard.some((el) => {
-                el == ''
-            })) {
+     
+        let win = [[]]
 
 
-            board.gameboard.forEach((el, i) => {
-
-                if (el == playerOne.pSymbol) {
-                    playerOneValues.push(i)
-                } else {
-                    playerTwoValues.push(i)
-                }
-
-            });
-
-
-            win.forEach((el, i) => {
-
-                // if (el.join('').includes(playerOneValues.join(''))) {
-                //  winner = "player one"
-                 
-                //  return winner
-                    
-                // } else if (el.join('').includes(playerTwoValues.join(''))) {
-                //     winner = "player two"
-
-                //   return winner
-                // }
-                console.log(el.join('').includes(playerOneValues.join('')))
-
-            })
-
-            console.log(winner)
-
-        }
-
+    
 
 
 
