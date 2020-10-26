5596f20725aa958a7878ccb5d3cc1cd10350cd57
peterrobert <pwambui93@gmail.com>
2020-10-26 12:53:22 +0300

    created tests for the player

---
+++ src/tests/players.test.js
@@ -0,0 +1,15 @@
+const player = require('../index');
+const testPlayer = player
+
+
+test('it should return the player name and the player symbol', () => {
+
+    let player1 = ["peter", "x"];
+
+    expect(testPlayer.Players(player1[1], player1[0])).toStrictEqual({
+        "pSymbol": "x",
+        "name": "peter"
+    })
+    
+});
+
