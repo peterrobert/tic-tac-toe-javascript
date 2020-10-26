928a7a6cfc695e2eecb07462fd2690ffc7ea61ae
peterrobert <pwambui93@gmail.com>
2020-10-26 12:38:38 +0300

    created the test for the board

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ function GameBoard() {
     ['', '', ''],
   ];
 
-  function boardFull() {
+  function boardFull(e = gameboard) {
     let isFull = false;
-    if (gameboard[0].includes('') || gameboard[1].includes('') || gameboard[2].includes('')) {
+    if (e[0].includes('') || e[1].includes('') || e[2].includes('')) {
       isFull = false;
     } else {
       isFull = true;
@@ -130,7 +130,9 @@ function GameLogic(playerOne, playerTwo, board) {
   }
 
   function checkGame() {
-    const { gameboard } = board;
+    const {
+      gameboard
+    } = board;
     // horizontal
     for (let i = 0; i < 3; i += 1) {
       if (allThreeEqual(gameboard[i][0], gameboard[i][1], gameboard[i][2])) {
@@ -160,6 +162,8 @@ function GameLogic(playerOne, playerTwo, board) {
     currentPlayer,
   };
 }
+
+module.exports = {GameBoard, Players, GameLogic}
 
 window.onload = () => {
   document.getElementById('start_reset_button').value = 'start';
